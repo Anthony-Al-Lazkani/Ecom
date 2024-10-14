e10ed8dd712d6ae75aa2a2cd24f7de7f82e4a503
Anthony Al Lazkani <anthonylazkani.22@gmail.com>
2024-10-15 00:07:17 +0300

    Redesign done

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,11 +1,22 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
-import { useContext, useState, React } from "react";
+import { useContext, useState } from "react";
+import React from "react";
 import { AuthContext } from "../../context/authContext";
 import { motion } from "framer-motion";
+import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
 
 function Navbar() {
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
   const { isLoggedIn, Logout } = useContext(AuthContext);
+
+  const handleMouseEnter = () => {
+    setDropdownVisible(true);
+  };
+
+  const handleMouseLeave = () => {
+    setDropdownVisible(false);
+  };
 
   return (
     <motion.nav
@@ -13,13 +24,43 @@ function Navbar() {
       initial={{ y: "-100%", opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       exit={{ y: -"100%", opacity: 0 }}
-      transition={{duration : .7 , ease : "easeInOut"}}
+      transition={{ duration: 0.7, ease: "easeInOut" }}
     >
       <div className="logo">
-        <h3>Ecom</h3>
+        
       </div>
       <div className="links">
-        <Link to="/">Home</Link>
+        <div
+          className="Drop"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <Link to="/">
+            Home{" "}
+            {isDropdownVisible ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
+          </Link>
+          {isDropdownVisible && (
+            <motion.div
+              className="DropMenu"
+              initial={{ opacity: 0, y: -20 }} // Start above with opacity 0
+              animate={{ opacity: 1, y: 0 }} // Fade in and move to normal position
+              exit={{ opacity: 0, y: -20 }} // Fade out and move back up
+              transition={{ duration: 0.3 }}
+            >
+              <ul>
+                <li>
+                  <a href="#section2">Bedroom</a>
+                </li>
+                <li>
+                  <a href="">Kitchen</a>
+                </li>
+                <li>
+                  <a href="">Living Room</a>
+                </li>
+              </ul>
+            </motion.div>
+          )}
+        </div>
         <Link to="/">About</Link>
         <Link to="/">Contact</Link>
         <button className="getStartedBtn">
--- frontend/src/pages/homepage/Home.js
+++ frontend/src/pages/homepage/Home.js
@@ -1,8 +1,12 @@
 import "./Home.css";
 import axios from "axios";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Home = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isHovered1, setIsHovered1] = useState(false);
+  const [isHovered2, setIsHovered2] = useState(false);
   return (
     <>
       <motion.section
@@ -36,24 +40,57 @@ const Home = () => {
         </div>
       </motion.section>
 
-      <section className="secondSection">
-        <div className="title">
-          <div className="titleDesc">
+      <motion.section
+        className="secondSection"
+        id="section2"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.7, ease: "easeInOut" }}
+      >
+        <div className="secondSectionContainer">
+          <div className="secondSectiontitle">
             <h1>
-              Ecom is the Top Notch modern luxury designer, ready to elevate
-              your space into perfectness
+              Ecom is the Top Notch modern luxury designer , ready to elevate
+              your space into elegance.
             </h1>
           </div>
-        </div>
-        <div className="content">
-          <div className="contentBox">
-            <div className="contents">
-              <h1>H11</h1>
+          <div className="secondSectioncontent">
+            <div className="contentCardContainer">
+              <div
+                className="contentCard card1"
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+              >
+                {isHovered ? <h1>Living Room</h1> : <div></div>}
+              </div>
             </div>
-            <div className="contents">H22</div>
-            <div className="contents">H33</div>
+
+            <div className="contentCardContainer">
+              <div
+                className="contentCard card2"
+                onMouseEnter={() => setIsHovered1(true)}
+                onMouseLeave={() => setIsHovered1(false)}
+              >
+                {isHovered1 ? <h1>Kitchen</h1> : <div></div>}
+              </div>
+            </div>
+
+            <div className="contentCardContainer">
+              <div
+                className="contentCard card3"
+                onMouseEnter={() => setIsHovered2(true)}
+                onMouseLeave={() => setIsHovered2(false)}
+              >
+                {isHovered2 ? <h1>Bedroom</h1> : <div></div>}
+              </div>
+            </div>
           </div>
         </div>
+      </motion.section>
+
+      <section className="thirdSection">
+      <div className="thirdSectionBG"></div>
       </section>
     </>
   );
